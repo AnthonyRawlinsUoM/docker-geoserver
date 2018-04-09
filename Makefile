ac6c4eb60668919fab82c5bcc762aaaf9606e682
Unknown <anthony.rawlins@unimelb.edu.au>
2018-04-09 15:42:46 +1000

    Build fixes

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-all: build
+all: pull
 
 build:
 	@docker build -t anthonyrawlinsuom/lfmc-geoserver .
 
 install:
 	@docker push anthonyrawlinsuom/lfmc-geoserver
-
+	
+pull:
+	@docker pull anthonyrawlinsuom/lfmc-geoserver
+	
 clean:
-	@docker rmi anthonyrawlinsuom/lfmc-geoserver
+	@docker rmi --force anthonyrawlinsuom/lfmc-geoserver
--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ docker kill postgis
 docker rm postgis
 
 #$DATA_DIR=~/geoserver_data
-export DATA_DIR=/media/arawlins/Backups/DataSources/geoserver_data
-if [ ! -d $DATA_DIR ]
+
+if [ ! -d $LFMC_GEOSERVER_DATA_DIR ]
 then
-    mkdir -p $DATA_DIR
+    mkdir -p $LFMC_GEOSERVER_DATA_DIR
 fi
 
 docker run --name "postgis" -p 5432:5432 -d -t kartoza/postgis:9.4-2.1
@@ -16,7 +16,7 @@ docker run --name "postgis" -p 5432:5432 -d -t kartoza/postgis:9.4-2.1
 docker run \
 	--name=geoserver \
 	--link postgis:postgis \
-        -v $DATA_DIR:/opt/geoserver/data_dir \
+        -v $LFMC_GEOSERVER_DATA_DIR:/opt/geoserver/data_dir \
 	-p 8080:8080 \
 	-d \
 	-t anthonyrawlinsuom/lfmc-geoserver
